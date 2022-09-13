0457eca22f31600c0ac6ec65e612fa1a193d6f49
Arsema-Zeweldi <arsugeb@gmail.com>
2022-09-13 20:39:10 +0000

    Task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,9 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    def delete(self, obj=None):
+        """delete obj from __objets"""
+
+        if obj:
+            del(self.__objects["{}.{}".format(type(obj).__name__, obj.id)])
+            del(obj)
